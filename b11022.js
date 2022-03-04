303f89157288a9fb28150fdf6d71f88c2fd01cc3
Hojun-Son <80058141+Hojun-Son@users.noreply.github.com>
2022-03-04 17:49:57 +0900

    Update b11022.js

--- b11022.js
+++ b11022.js
@@ -20,5 +20,15 @@ rl.on("line", function (line) {
 
     console.log("Case #" + i + ":" + " " + a + " + " + b + " = " + c);
   }
+///////////////////////////////////////////////////////////////////////////권장 풀이  
+//   for (let i = 1; i <= input[0]; i++) {
+//     let numbers = input[i].split(' ');
+    
+//     let num1 = Number(numbers[0]);
+//     let num2 = Number(numbers[1]);
+    
+//     console.log(`Case #${i}: ${num1} + ${num2} = ${num1 + num2}`);
+// }
+///////////////////////////////////////////////////////////////////////////  
   process.exit();
 });
