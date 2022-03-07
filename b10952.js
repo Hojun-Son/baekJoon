68981b7c5eddeff659dd75f2c893759ba3737a45
Hojun-Son <80058141+Hojun-Son@users.noreply.github.com>
2022-03-07 22:39:30 +0900

    Update b10952.js

--- b10952.js
+++ b10952.js
@@ -1,4 +1,25 @@
-//A+B -5
+// //A+B -5///////my answer
+// const readline = require("readline");
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// let num = [];
+// rl.on("line", function (line) {
+//   num.push(line);
+// }).on("close", function () {
+//   for (i = 0; i < num.length; i++) {
+//     let number = num[i].split(" ");
+//     a = Number(number[0]);
+//     b = Number(number[1]);
+//     if (a !== 0 && b !== 0) {
+//       console.log(a + b);
+//     } else break;
+//   }
+//   process.exit();
+// });
+////////////////////////////////////////////////////
 const readline = require("readline");
 
 const rl = readline.createInterface({
@@ -9,14 +30,19 @@ let num = [];
 rl.on("line", function (line) {
   num.push(line);
 }).on("close", function () {
-  for (i = 0; i < num.length; i++) {
-    let number = num[i].split(" ");
-    a = Number(number[0]);
-    b = Number(number[1]);
-    if (a !== 0 && b !== 0) {
-      console.log(a + b);
-    } else break;
+  let i = 0;
+  answer = "";
+  while (i <= num.length - 1) {
+    let a = parseInt(num[i].split(" ")[0]);
+    let b = parseInt(num[i].split(" ")[1]);
+    if (a !== 0 || b !== 0) {
+      answer += `${a + b}` + "\n";
+    } else {
+      break;
+    }
+    i++;
   }
+  console.log(answer);
   process.exit();
 });
-////////////////////////////////////////////////////
+
