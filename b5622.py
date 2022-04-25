bf7bd32b349eec96265080bef1895ba3fdfcc52f
Hojun-Son <80058141+Hojun-Son@users.noreply.github.com>
2022-04-25 20:16:09 +0900

    Update b5622.py

--- b5622.py
+++ b5622.py
@@ -1,8 +1,12 @@
-dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
-a = input()
-ret = 0
-for j in range(len(a)):
-    for i in dial:
-        if a[j] in i:
-            ret += dial.index(i)+3
-print(ret)
+#다이얼
+
+word = input()
+
+dial = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+
+sec = 0
+for i in range(len(word)):
+    for j in dial:
+        if word[i] in j:
+            sec += dial.index(j)+3 #1번이 2초, 2번(인덱스:0)은 3초, 3번(인덱스:1)은 4초... -> 인덱스+3초
+print(sec)
