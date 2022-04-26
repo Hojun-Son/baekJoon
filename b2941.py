1bdbeda631b81ee0d9318aee1dbd987caa3c911f
Hojun-Son <80058141+Hojun-Son@users.noreply.github.com>
2022-04-26 22:40:57 +0900

    Update b2941.py

--- b2941.py
+++ b2941.py
@@ -1,13 +1,21 @@
+# 내 풀이
+# word = input()
+# cnt=0
+# croatia_alphabet=['c-','c=','dz=','d-','lj','nj','s=','z=']
+# for i in croatia_alphabet:
+#         if i in word:
+#             cnt +=word.count(i)
+#             #print('i는',i, ' 갯수',cnt)
+#             word = word.replace(i,',')
+#             #print(word)
+
+
+# word = word.replace(',','')
+# print(cnt+len(word))
+
 word = input()
-cnt=0
-croatia_alphabet=['c-','c=','dz=','d-','lj','nj','s=','z=']
-for i in croatia_alphabet:
-        if i in word:
-            cnt +=word.count(i)
-            #print('i는',i, ' 갯수',cnt)
-            word = word.replace(i,',')
-            #print(word)
+croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
-
-word = word.replace(',','')
-print(cnt+len(word))
+for i in croatia :
+    word = word.replace(i, '*')  # input 변수와 동일한 이름의 변수
+print(len(word))
