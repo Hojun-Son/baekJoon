a3da8f42cd34a9d0828179391def1fb1d2b01b8f
Hojun-Son <80058141+Hojun-Son@users.noreply.github.com>
2022-05-10 17:45:09 +0900

    Update b1978.py

--- b1978.py
+++ b1978.py
@@ -1,13 +1,13 @@
 #소수찾기
 n = int(input())
-numbers = map(int, input().split())
-sosu = 0
-for num in numbers:
+cnt = 0
+nums = map(int, input().split(' '))
+for num in nums:
     error = 0
-    if num > 1 :
-        for i in range(2, num):  # 2부터 n-1까지
+    if num > 1:
+        for i in range(2, num): # 2부터 n-1까지
             if num % i == 0:
-                error += 1  # 2부터 n-1까지 나눈 몫이 0이면 error가 증가
+                error += 1 # 2부터 n-1까지 나눈 몫이 0이면 error가 증가
         if error == 0:
-            sosu += 1  # error가 없으면 소수.
-print(sosu)
+            cnt += 1 # error가 없으면 소수.
+print(cnt)
