1467cb78e49eb8a97e4371b759085a83ffd00f16
Hojun-Son <ddm9396@gmail.com>
2022-10-06 11:52:36 +0900

    Create 1427.py
    소트인사이드

---
+++ 1427.py
@@ -0,0 +1,13 @@
+##########################내풀이
+import sys
+input = sys.stdin.readline
+
+N = input()
+n_list = []
+n_str = ""
+for i in range(0, len(N) - 1):
+    n_list.append(int(N[i]))
+n_list.sort(reverse=True)
+for j in range(0, len(N) - 1):
+    n_str += str(n_list[j])
+print(n_str)
