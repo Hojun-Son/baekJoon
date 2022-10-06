ca520668d31d5f27a15dbce256dd53af1820997b
Hojun-Son <ddm9396@gmail.com>
2022-10-06 12:28:44 +0900

    Update b1165(좌표정렬하기).py

--- b1165(좌표정렬하기).py
+++ b1165(좌표정렬하기).py
@@ -23,5 +23,5 @@ for _ in range(0, N):
     coordinate_list.append(list(map(int, input().split(" "))))
 coordinate_list.sort()
 for i in range(0, len(coordinate_list)):
-    print(coordinate_list[i][0], coordinate_list[i][1]) ####for문 안쓰는게 
+    print(coordinate_list[i][0], coordinate_list[i][1]) ####for문 안쓰는게 경제적
 
