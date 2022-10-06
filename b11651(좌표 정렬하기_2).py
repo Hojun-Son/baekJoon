17947b8d44532d7a6ed588465a364f8c691d9802
Hojun-Son <ddm9396@gmail.com>
2022-10-06 18:39:39 +0900

    Update b11651(좌표 정렬하기_2).py

--- b11651(좌표 정렬하기_2).py
+++ b11651(좌표 정렬하기_2).py
@@ -5,10 +5,23 @@ input = sys.stdin.readline
 N = int(input())
 coordinate_list = []
 for _ in range(0, N):
-    temp_list = list(map(int, input().split(" ")))
-    reversed_list = reversed(temp_list)
-    coordinate_list.append(list(reversed_list))
+    coordinate_list.append(list(reversed(list(map(int, input().split(" "))))))
 
 coordinate_list.sort()
 for i in range(0, len(coordinate_list)):
     print(coordinate_list[i][1], coordinate_list[i][0])
+
+# import sys
+
+# input = sys.stdin.readline
+
+# N = int(input())
+# coordinate_list = []
+# for _ in range(0, N):
+#     temp_list = list(map(int, input().split(" ")))
+#     reversed_list = reversed(temp_list)
+#     coordinate_list.append(list(reversed_list))
+
+# coordinate_list.sort()
+# for i in range(0, len(coordinate_list)):
+#     print(coordinate_list[i][1], coordinate_list[i][0])
